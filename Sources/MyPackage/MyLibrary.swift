21bd89a335a8ad496bbe6f8108fa9ebff7b9cd2e
Dhruv Govani <dhruvgovani@icloud.com>
2024-02-10 17:08:58 +0530

    SPM Created

---
+++ Sources/MyPackage/MyLibrary.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dhruv Govani on 10/02/24.
+//
+
+import Foundation
+
+public struct MyLibrary {
+    public init() {}
+    
+    public func helloWorld() {
+        print("Hello, World!")
+    }
+}
